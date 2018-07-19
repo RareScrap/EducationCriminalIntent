793fbd35f90179cc321657ca7329e64390d66205
RareScrap <RareScrap@users.noreply.github.com>
2018-07-19 10:36:56 +0800

    Дочитана глава 11

--- app/src/main/java/com/apptrust/educationcriminalintent/CrimeListFragment.java
+++ app/src/main/java/com/apptrust/educationcriminalintent/CrimeListFragment.java
@@ -99,7 +99,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivityForResult(intent, UPDATE_ITEM_REQUEST_CODE);
         }
     }
---
+++ app/src/main/java/com/apptrust/educationcriminalintent/CrimePagerActivity.java
@@ -0,0 +1,57 @@
+package com.apptrust.educationcriminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.LinkedHashMap;
+import java.util.UUID;
+
+
+/**
+ * @author RareScrap
+ */
+public class CrimePagerActivity extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID =
+            "com.apptrust.educationcriminalintent.crime_id";
+
+    private ViewPager mViewPager;
+    private LinkedHashMap<String, Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = CrimeLab.get(CrimePagerActivity.this).getCrimeByIndex(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        mViewPager.setCurrentItem(CrimeLab.get(this).getPosition(crimeId));
+    }
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+}
